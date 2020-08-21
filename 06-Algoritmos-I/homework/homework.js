3f87db2a6ccc4a226d3bd0c7042a6adeed3efe0e
Franverri <francoetche@gmail.com>
2020-08-21 09:23:49 -0300

    Fix hw 06

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -3,7 +3,7 @@
 function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
-  // Ej: factorear(180) --> [2, 2, 3, 3, 5] Ya que 2x2x3x3x5 = 180 y son todos números primos
+  // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
 }
--- 06-Algoritmos-I/homework/tests/AlgoritmosI.test.js
+++ 06-Algoritmos-I/homework/tests/AlgoritmosI.test.js
@@ -8,9 +8,9 @@ const {
 
 describe('factorear(num)', function() {
   it('Debería devolver ...?', function() {
-    expect(factorear(180)).toEqual([2,2,3,3,5]);
-    expect(factorear(32)).toEqual([2,2,2,2,2]);
-    expect(factorear(33)).toEqual([3,11]);
+    expect(factorear(180)).toEqual([1,2,2,3,3,5]);
+    expect(factorear(32)).toEqual([1,2,2,2,2,2]);
+    expect(factorear(33)).toEqual([1,3,11]);
     expect(factorear(1)).toEqual([1]);
   });
 });
