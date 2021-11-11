d5c56a023d8857a52e2f8476beaf7b8cd56f0658
Franverri <francoetche@gmail.com>
2021-11-11 15:02:20 -0300

    Finish extra testing hw

--- EXTRA-Testing/homework/homework.js
+++
@@ -1,9 +0,0 @@
-function checkSeatStatus(row, number) {
-  if(typeof row !== 'string') throw new TypeError('First parameter is not a string');
-  if(typeof number !== 'number') throw new TypeError('Second parameter is not a number');
-
-}
-
-module.exports = {
-  checkSeatStatus
-}
