14067c7878b6d62ab11bbcef471452791164ce61
unknown <phelanguille@gmail.com>
2022-04-25 17:06:48 -0300

    Clase 1 M 1

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  // algo
-}
+  var decimal = 0 ; 
+for ( var i = 0 ; i < num.length ; i++ ) {
+decimal = decimal + num[i] * 2 ** (num.length -1 -i) ; }
+return decimal ; }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var binario = '' ; 
+  while ( num > 0 ) { 
+    binario = (num % 2 ) + binario ; 
+    num = Math.floor(num / 2) ; 
+  }
+  return binario ; 
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
