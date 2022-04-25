86e56d96e990fcaf9023843d696a4cb583b23e9c
Hernán García <84438812+hernancur@users.noreply.github.com>
2022-04-25 11:56:35 -0300

    mi primera homework

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,17 +1,15 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  // algo
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
